63be0d516f819cc877e75d169375bf210e9f213d
Paul Adams <Paul Adams>
2017-03-31 16:47:13 -0400

    added Results component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import DevTools from 'mobx-react-devtools'
 import {observer} from 'mobx-react'
 import CarbonFootprint from './CarbonFootprint.js'
-import {Button} from 'antd'
+import Results from './Results.js'
+import {Layout} from 'antd'
+const {Header, Footer, Sider, Content} = Layout
 import './App.css';
 
 
 const App = observer(class App extends Component {
   render() {
     return (
-      <div className="App">
-        <CarbonFootprint/>
-        <Button type="primary">test</Button>
+      <div>
+        <Layout>
+          <Sider className="side-bar">
+            <CarbonFootprint/>
+          </Sider>
+          <Layout>
+            <Header className="title">Carbon Footprint Calculator</Header>
+            <Content className="display-results">
+              Content
+            </Content>
+          </Layout>
+        </Layout>
        <DevTools/>
       </div>
     );
---
+++ src/Results.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {observer} from 'mobx-react'
+
+const Results = observer(class Results extends Component{
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+})
+
+export default Results
