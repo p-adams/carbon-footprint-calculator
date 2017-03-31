88896028b3c2d9763c947922a32011d119885700
Paul Adams <Paul Adams>
2017-03-30 23:16:29 -0400

    added antd dependency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DevTools from 'mobx-react-devtools'
 import {observer} from 'mobx-react'
 import CarbonFootprint from './CarbonFootprint.js'
+import {Button} from 'antd'
 import './App.css';
 
 
@@ -10,6 +11,7 @@ const App = observer(class App extends Component {
     return (
       <div className="App">
         <CarbonFootprint/>
+        <Button type="primary">test</Button>
        <DevTools/>
       </div>
     );
